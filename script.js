5db7dd61585249bb7dc1660ab0f77f90dc71385c
Wesley.J <westwesley2022gmail.com>
2026-08-03 14:15:01 +0300

    create functions for saving events and getting stored events

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 // Key used for storing our events array in localStorage
 const STORAGE_KEY = 'community_hub_events';
+
+//helper function to get events from localStorage.
+//returns an array of events objects
+function getStoredEvents() {
+  const eventsData = localStorage.getItem(STORAGE_KEY);
+  return eventsData ? JSON.parse(eventsData) : getInitialDefaultEvents();
+}
+
+// Helper function to save an array of events to LocalStorage.
+function saveEvents(events) { //to local storage
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(events));
+}
